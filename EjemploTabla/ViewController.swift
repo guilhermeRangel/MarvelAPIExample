e062b295eb5096a00d79509a538fbaeae933b5ca
Mauricio <mauricio.vasquez@evol-tech.com>
2015-04-23 20:43:23 -0500

    Initial Commit

---
+++ EjemploTabla/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjemploTabla
+//
+//  Created by Mauricio Vasquez on 4/23/15.
+//  Copyright (c) 2015 Mauricio Vasquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
